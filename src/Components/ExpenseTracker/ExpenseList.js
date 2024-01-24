6017fd06ef1323259f2f019c08fe24b388788a50
ashwin695 <guptaashwin695@gmail.com>
2024-01-24 16:53:00 +0530

    seventh commit

---
+++ src/Components/ExpenseTracker/ExpenseList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./ExpenseList.css"
+
+const ExpenseList = (props) => {
+    return(
+        <div>
+            <h3 className="expenses-header">Expenses</h3>
+            {
+                props.expenses.length === 0 ? (
+                    <p className="no-expenses">No Expenses added yet.</p>
+                ) : (
+                    <ul className="expenses-list">
+                        {
+                            props.expenses.map((expenses, index) => (
+                                <li key={index} className="expense-item">
+                                    Money Spent: {expenses.moneySpent} <br></br>
+                                    Description: {expenses.description} <br></br>
+                                    Category: {expenses.category}
+                                </li>
+                            ))
+                        }
+                    </ul>
+                )
+            }
+        </div>
+    )
+}
+
+export default ExpenseList
